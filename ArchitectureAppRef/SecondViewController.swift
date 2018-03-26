6ff262751ba76c0340d6c40e6974895d8ff85d01
vincent_g <vincent.grossier@infostrates.fr>
2018-03-26 09:52:00 +0200

    Initial Commit

---
+++ ArchitectureAppRef/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ArchitectureAppRef
+//
+//  Created by vincent on 26/03/2018.
+//  Copyright © 2018 Infostrates. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
